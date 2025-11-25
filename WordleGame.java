717f75fce37ae961bc119901a0a4e91c88d6b5db
Ihnat Zharikhin <ignazhar@gmail.com>
2025-11-25 10:54:03 +0000

    added main logic

--- WordleGame.java
+++ WordleGame.java
@@ -1,19 +1,38 @@
 public class WordleGame {
     private static int guessNumber = 0;
-    private static final int guessLimit = 10;
+    private static final int guessLimit = 6;
     
     public static void main(String[] args) {
         WordDatabase database = new WordDatabase();
-        database.printWords();
 
-        String targetWord = database.chooseWord(); 
+        String targetWord = WordDatabase.chooseWord(); 
 
-        System.out.println(targetWord);
+        WordInput input = new WordInput();
 
-        System.out.println(database.isValid("wrest"));
+        while (guessNumber < guessLimit) {
+            guessNumber ++;
+            System.out.print("Enter word: (" + guessNumber + " out of " + guessLimit + "):");
+            try {
+                String guessWord = input.readWord(database);
+                 
+                String result = WordHandler.compare(guessWord, targetWord);
 
-        // while (guessNumber < guessLimit) {
-        //     String guessWord = try { readWord() } catch 
-        // }
+                System.out.println(result);
+
+                if (result.equals("+++++")) {
+                    System.out.println("Won game in " + guessNumber + " guesses !!!");
+                    return;
+                }
+            } catch (IllegalArgumentException s) {
+                System.out.println("Word input error: " + s.getMessage());
+                // giving another attempt
+                guessNumber --;
+                continue;
+            }
+        }
+
+        System.out.println("Couldn't guess the word! The answer was " + targetWord);
+
+        input.close();
     }
 }
